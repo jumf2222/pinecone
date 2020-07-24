d337e9fdf69674f5145590021d788db953debbc0
Latxhman <55321722+Latxhman@users.noreply.github.com>
2020-07-24 12:59:55 -0400

    Dashboard table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from "@angular/core";
+import { Schedule } from '../definitions';
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  styleUrls: ["./home.component.scss"],
 })
+
 export class HomeComponent implements OnInit {
+
+  schedules: Schedule[] = [];
+  displayedColumns = ['name', 'year', 'term'];
 
   constructor() { }
 
